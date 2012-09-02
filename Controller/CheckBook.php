7db472f18b4c158f37572dcd11387dd3875cb751
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2012-09-02 09:50:30 -0700

    Added view parent class

--- Controller/CheckBook.php
+++ Controller/CheckBook.php
@@ -11,6 +11,7 @@ class CheckBook{
 	private $username = null;
 	private $userid = null;
 	private $db = null;
+	private $links = array( 'Home' => 'Home', 'Reports' => 'Reports' ,'Settings' => 'Settings');
 
 	public function __construct($user){
 		//We can populate and anything else from knowing the user to lookup
@@ -19,6 +20,16 @@ class CheckBook{
 		$this->username = $user;
 		$this->userid = $this->db->matchNameToID($user);
 
+	}
+
+	public function valid($user){
+		if(is_null($this->db)){return false;}
+		//If the name exists then the function returns false so we ! it to make true
+		return !$this->db->checkAvailableName($user);
+	}
+
+	public function render(){
+		//Will render the page using the view functions and such
 	}
 
 }
---
+++ View/View.php
@@ -0,0 +1,69 @@
+<?php
+
+/**************************
+/*Parent for all views, provides common functions like
+/*logout,formatDate,displayMenus, 
+/**************************/
+
+class View { 
+
+	function formatDate($date){
+		//"2012-08-30 18:47:37" -> 6:47pm on 08/30/12
+		$midway = explode(' ',$date);
+		$dString = explode('-',$midway[0]);
+		$tString = explode(':',$midway[1]);
+		$hour = intval($tString[0]);
+		$ampm = ($hour > 11 ? 'pm' : 'am');
+		$hour = strval($hour % 12);
+		return $hour . ':'  . $tString[1] . $ampm . ' on ' . $dString[1] . '/' . $dString[2] . '/' . substr($dString[0],-2);
+
+	}
+
+	function getJustDate($date){
+		//"2012-08-30 18:47:37" -> 08/30/12
+		$midway = explode(' ',$date);
+		$dString = explode('-',$midway[0]);
+		return $hour . ':'  . $tString[1] . $ampm . ' on ' . $dString[1] . '/' . $dString[2] . '/' . substr($dString[0],-2);
+	}
+
+	function displayMenus($links){
+		//Home page links to settings, checkbook and reports
+		echo '<div class = "Menus">';
+		echo '<div class = "largespacer"></div>';
+
+		//use a horizontal list
+		echo '<ul class = "Menu">';
+	
+		foreach ($links as $link) {
+			echo '<li class="Menu">';
+			echo '<form name = "' . $link . '" method="post" action = "../BudgetBuddy/Page/' . str_replace(' ', '', $link) . 'View.php">';
+			echo '<button class = "Menu"  type="submit">'. $link .'</button>';
+			echo '</form>';
+			echo '</li><span class = "MenuSpacer"><br /></span>';
+		}
+		
+
+		echo '</ul>';
+		echo '</div>';
+	}
+
+	function logout(){
+		//Send used back to the Login page and destroys the session
+		echo '<div class = Logout">';
+		echo '<form name = "logout" method="post" action = "../../BudgetBuddy/Logout.php">';
+		echo '<button class = "Logout"  type="submit">Logout</button>';
+		echo '</form>';
+		echo '</div>';
+
+	}
+
+
+
+}
+
+
+
+
+
+
+?>
