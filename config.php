adba64459d000afc4f282a4f67ddc9c744b1b1d0
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-05-26 12:24:12 -0700

    Added example config file to address issue #1, this file should be stored one directory above wherever the budget buddy files sit. The table structure for the sql is coming, but my wampserver and mysql are having trouble connecting because something else is blocking the port and I'm not sure why. Working on resolving this

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("DATABASE_NAME", "BudgetBuddy");
+define("DATABASE_HOST", "127.0.0.1");
+define("DATABASE_PASS", "");
+define("DATABASE_USER", "");
+define("DEFAULT_THEME", "Night");
+ini_set('date.timezone','America/New_York');
+?>
